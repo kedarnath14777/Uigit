85f2fe2760c3758bfbc78b7c47941b1c6422e8ac
kedarnath3699 <kedarnath.a3699@gamil.com>
2024-08-29 15:01:52 +0530

    this 1

---
+++ 03-variables2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+person1=kedarnath
+person2=kishore
+
+echo "$person1 :: Hello $persion2"
+echo "$persion2 :: Hello $person1"
+echo "$person1 :: How are  $persion2"
+echo "$persion2 ::  am good $person1 "
