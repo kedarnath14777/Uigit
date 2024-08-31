88430b21c8c8b99bf6e80475b3e79ac6b3be86bd
kedarnath3699 <kedarnath.a3699@gamil.com>
2024-08-31 15:27:04 +0530

    ji

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -8,3 +8,9 @@ echo "to check the current user" $USER
 
 echo " check the current working dir" $PWD 
 
+echo " home dir of the current user is " $home
+
+echo " Pid of the urrent shell script is ;" $$
+
+echo " name of the current shell script is :" $0
+
