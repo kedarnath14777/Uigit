55eba78116b1267437aa46762690493815caaf2a
kedarnath3699 <kedarnath.a3699@gamil.com>
2024-08-31 17:37:48 +0530

    ji

---
+++ 14-basic-installation.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+USER=$(id-u)
+
+if($USER -eq 0)
+then 
+    echo "you are super user"
+else
+    echo " need super user access to de this"
+    exit 1 # manually exiting 
+fi 
+
+dnf install macen -y 
+
+if ($? -eq 0 )
+then 
+    echo "installed successfully"
+else
+    echo " not installed"
+    exit 1 
+fi 
