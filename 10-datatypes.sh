0d0f4cbff97f9906ad6f7dcf6f112b9858775636
kedarnath3699 <kedarnath.a3699@gamil.com>
2024-08-31 17:23:49 +0530

    ji

--- 10-datatypes.sh
+++ 10-datatypes.sh
@@ -6,3 +6,20 @@ num2=$2
 sum=$((num1+num2))
 
 echo " sum of 2 nums is " ${sum}
+
+
+# Sub
+
+Sub=$((num1-num2))
+
+echo " dub of 2 num is " $Sub
+
+# munl
+
+mul=$((num1*num2))
+
+#Did
+
+did=$((num1/num2))
+
+echo "did of 2 num is " $did 
