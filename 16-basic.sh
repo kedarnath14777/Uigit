44fa89f970a1aca1f77e97d1c55cade90edc20f7
kedarnath3699 <kedarnath.a3699@gamil.com>
2024-09-02 08:49:03 +0530

    hai

--- 16-basic.sh
+++ 16-basic.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
-userid=$(id -u)
+USERID=$(id -u)
 
-time=$(date +%F-%H-%M-%S)
+TIME_STAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1 )
+LOG_FILE=/tmp/$SCRIPT_NAME-$TIME_STAMP.log
 
-echo $SCRIPT_NAME
+# super user validate 
+if [ $USERID -eq 0 ]
+then 
+    echo " u r super user "
+else
+    echo " need super user access"
+    exit 1 # manually exiting 
+fi 
+
+VALIDATE_FUNCTION(){
+    if [ $? -eq 0 ]
+    then
+        echo "$2 .. Successfully.."
+    else
+        echo "$@.. Failed" 
+
+}
+
+dnf install mysql -y 
+VALIDATE_FUNCTION $? "instaling mysql"
+
+dnf install git -y 
+VALIDATE_FUNCTION $? "instaling git"
+
+dnf install maven -y 
+VALIDATE_FUNCTION $? "instaling maven "
