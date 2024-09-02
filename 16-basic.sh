84f4f342cebbe1c5d8f0ef8278dc525e82fc29f2
kedarnath3699 <kedarnath.a3699@gamil.com>
2024-09-02 08:56:17 +0530

    hai

--- 16-basic.sh
+++ 16-basic.sh
@@ -5,31 +5,36 @@ USERID=$(id -u)
 TIME_STAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1 )
 LOG_FILE=/tmp/$SCRIPT_NAME-$TIME_STAMP.log
+# colour enabling 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 # super user validate 
 if [ $USERID -eq 0 ]
 then 
-    echo " u r super user "
+    echo -e "$G u r super user $N "
 else
-    echo " need super user access"
+    echo -e "$R need super user access $N"
     exit 1 # manually exiting 
 fi 
 
 VALIDATE_FUNCTION(){
     if [ $? -eq 0 ]
     then
-        echo "$2 .. Successfully.."
+        echo -e " $G $2 .. Successfully.. $N"
     else
-        echo "$@.. Failed" 
+        echo -e  " $R  $@.. Failed $N" 
     fi 
 
 }
 
-dnf install mysql -y 
+dnf install mysql -y &>> LOG_FILE
 VALIDATE_FUNCTION $? "instaling mysql"
 
-dnf install git -y 
+dnf install git -y &>> LOG_FILE
 VALIDATE_FUNCTION $? "instaling git"
 
-dnf install maven -y 
+dnf install maven -y &>> LOG_FILE
 VALIDATE_FUNCTION $? "instaling maven "
