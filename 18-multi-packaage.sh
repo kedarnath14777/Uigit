1b06b28ae9b9a104ed7226eed51a73760ddfb472
kedarnath3699 <kedarnath.a3699@gamil.com>
2024-09-02 10:26:23 +0530

    hai

--- 18-multi-packaage.sh
+++ 18-multi-packaage.sh
@@ -5,26 +5,35 @@ TIME=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$($0 | cut -d "." -f1 )
 logs=/tmp/$SCRIPT_NAME-$TIME.log 
 
+# colour enabling 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+
 echo $logs
 
 if [ $USER -eq 0 ]
 then 
-    echo "you are super user" 
+    echo -e "$G you are super user $N " 
 else
-    echo " need super user access to de this"
+    echo -e "$R  need super user access to de this $N "
     exit 1 # manually exiting 
 fi 
 MY_PACKAGES=$@
-echo "please enter the packages " $MY_PACKAGES
-
 for i in ${MY_PACKAGES[@]}
 do 
     dnf list installed $i  &>>logs
     if [ $? -eq 0 ]
     then 
-        echo "already installed $i" &>>logs
+        echo -e  "$Y already installed $i .. SKPPINNG " $N  &>>logs
     else 
-        dnf install $i -y 
+        dnf install $i -y  &>>logs
+        if [ $? -eq 0 ]
+        then
+            echo -e  " $G installing $i $N " 
     fi 
 done 
 
