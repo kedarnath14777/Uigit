91bfae3c7bdceda8da0d61ab13a0efd7e437ee26
kedarnath3699 <kedarnath.a3699@gamil.com>
2024-09-02 09:03:57 +0530

    hai

---
+++ 18-multi-packaage.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+USER=$(id -u)
+TIME=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$($0 | cut -d "." -f1 )
+logs=/tmp/$SCRIPT_NAME-$TIME.log 
+
+echo $logs
+
+
+
+if [ $USER -eq 0 ]
+then 
+    echo "you are super user"
+else
+    echo " need super user access to de this"
+    exit 1 # manually exiting 
+fi 
+
+echo "please enter the packages " $@
+
+
+validate_installation(){
+    if [ $1 -eq 0 ]
+    then    
+        echo "installed successfuly $2 "
+    else
+        echo "not installed $2"
+        exit 
+    fi 
+
+}
