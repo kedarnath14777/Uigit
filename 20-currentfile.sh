75a5b58665188027948cc14f8c164e50623a6714
kedarnath3699 <kedarnath.a3699@gamil.com>
2024-09-02 12:38:40 +0530

    hai

---
+++ 20-currentfile.sh
@@ -0,0 +1,13 @@
+#!bin/bash 
+
+course="devops with AWS"
+
+echo "before caling from another file $course"
+
+echo "pid is $$"
+
+./21-anotherfile.sh
+
+
+echo "before caling from another file $course"
+echo "pid is $$"
